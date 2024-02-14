e74e1dca09dbf790e22ef083afddb017ba678905
Hapsik1234 <dawidosxt@gmail.com>
2024-02-14 21:25:11 +0100

    Bolek postanowił usunąć repo xd

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+
 const { spawn } = require('child_process');
 
-const const_server_port = 3000;    // Websocket server
-const const_path_to_mcserver = "C:\\Users\\bolek\\Desktop\\WALTUH\\servers";  // Path to minecraft server
-const command = "cd "+ const_path_to_mcserver + "\\"+  + " && call run.bat\n";
+const const_server_port = "3000";    // Websocket server
+const const_path_to_mcserver ="/home/bolk/Pulpit/server/serverside/servers";  // Path to minecraft server
+const command = "cd "+ const_path_to_mcserver + "/"+  + " && sudo bash\n";
 
 const pass = "mMVahPSL2qcg0Q4d0LKC6A==";
 
@@ -11,7 +12,7 @@ const io = require('socket.io')(const_server_port, {
   cors: {
       orgin: ["http://big-idle.at.ply.gg:53454", "http://localhost"]
   }
-})
+});
 
 const color = {
   blue: "\x1b[34m \x1b[1m%s\x1b[0m",
@@ -29,7 +30,7 @@ class Server {
     this.state = "off";
     this.path = const_path_to_mcserver + '\\' + directory
     this.name = name;
-    this.cmd = spawn("cmd.exe", [""], { shell: true });
+    this.cmd = spawn("sudo bash", [""], { shell: true });
 
     ServerInstance(this);
   }
@@ -42,7 +43,7 @@ class Server {
 
 //Fun = new Server(Fun, "4fun");
 Surviv = new Server("Surviv", "Surviv");
-Fun = new Server("Fun", "Fun")
+//Fun = new Server("Fun", "Fun")
 
 //import { get } from 'http';
 
@@ -70,10 +71,18 @@ function ServerInstance(ServerObject) {
 
     // Handle requests from WebSocket
 
-    socket.on('request', (request) => {
-      console.log("Requsted " + request + " from " + socket.id);
-      requestHandler(request, ServerObject);
+    socket.on('request', (request, server) => {
+      console.log("Requested request " + request + " for " + server + " from " + socket.id);
+      requestHandler("request", request, ServerObject, server);
     });
+
+    socket.on('command', (request, server) => {
+      console.log("Requested command " + request + " for " + server + " from " + socket.id);
+      requestHandler("command", request, ServerObject, server);
+    });
+
+
+
 
     /* 
       CMD HANDLER
@@ -111,14 +120,16 @@ function ServerInstance(ServerObject) {
   });
 }
 
-function requestHandler(request, server){
-  //console.log("Received: " + request)
+function requestHandler(type, request, server, serverName){
+  console.log("Received: " + type +  " " + request + " " + server)
 
   //TODO: 
-  
+switch(type)
+{
+case "request":
+  if (server.name == serverName)
   switch(request) {
     case "server-start":
-
       if (server.state=="off") {
         server.changeState("starting")
         console.log("Starting server");
@@ -126,7 +137,7 @@ function requestHandler(request, server){
         // Starting minecraft server
 
         //cmd.emit
-        server.cmd.stdin.write("cd "+ const_path_to_mcserver + "\\"+ server.name + " && call run.bat\n");
+        server.cmd.stdin.write("cd "+ const_path_to_mcserver + "/"+ server.name + " && sudo bash run.sh\n");
         //cmd = new spawn(command, args, { shell: true });
       }
     
@@ -134,16 +145,22 @@ function requestHandler(request, server){
       
     case "server-stop":
 
-
       // Stoping minecraft server
 
+      server.changeState("off");
+      //Zmiana stanu serwera przed zatrzymaniem go jest celowa, bo inaczej masz remote code execution ze strony internetowej
       server.cmd.stdin.write('stop\n');
 
       console.log("Stopping server");
-      server.changeState("off");
+      
 
     break;
   }
+  case "command":
+  server.cmd.stdin.write(request + '\n');
+
+  break;
+ }
 }
 
 
